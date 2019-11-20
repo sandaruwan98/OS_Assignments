b1970e4e5080ac27a9ae1dcd2f4cc13b868cc1cf
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2019-11-20 18:26:53 +0530

    MyFree function part 1

--- malloc/mymalloc.h
+++ malloc/mymalloc.h
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
 #define SIZE 25000
 
 char memory[SIZE];
@@ -16,9 +16,11 @@ struct memoryLL
 
 typedef struct memoryLL node;
 
-node* start = NULL;;
+node *start = NULL;
+;
 
-node* NewNode(int freeOrAlloc, int stradr, int endAdr){
+node *NewNode(int freeOrAlloc, int stradr, int endAdr)
+{
     node *newnode = (node *)malloc(sizeof(node));
     newnode->prev = NULL;
     newnode->next = NULL;
@@ -28,67 +30,85 @@ node* NewNode(int freeOrAlloc, int stradr, int endAdr){
     return newnode;
 }
 
-
 void InsertNodeEnd(int freeOrAlloc, int stradr, int endAdr)
 {
 
-    node *newnode = NewNode(freeOrAlloc,stradr,endAdr);
-    if (start==NULL)
+    node *newnode = NewNode(freeOrAlloc, stradr, endAdr);
+    if (start == NULL)
     {
-        start=newnode;
-    }else
+        start = newnode;
+    }
+    else
     {
-        node* ptr = start;
-        while (ptr->next!=NULL)     
-            ptr=ptr->next;
-        ptr->next=newnode;
-        newnode->prev=ptr;        
+        node *ptr = start;
+        while (ptr->next != NULL)
+            ptr = ptr->next;
+        ptr->next = newnode;
+        newnode->prev = ptr;
     }
 }
 
-void PrintLL(){
-    node* ptr =start;
+void PrintLL()
+{
+    node *ptr = start;
 
-    while (ptr!=NULL)
+    while (ptr != NULL)
     {
-        printf("Free - %d \t start - %d \t end - %d\n",ptr->IsFree,ptr->startAdr,ptr->endAdr);
-        ptr=ptr->next;
+        printf("Free - %d \t start - %d \t end - %d\n", ptr->IsFree, ptr->startAdr, ptr->endAdr);
+        ptr = ptr->next;
     }
 }
 
-void FindFreeSpace(int size){
-   
+void FindFreeSpace(int size) {}
 
-}
-
-void MyMalloc(int size){
-     //find free partition
-    node* ptr = start;
-    while (ptr!=NULL)
+void MyMalloc(int size)
+{
+    //find free partition
+    node *ptr = start;
+    while (ptr != NULL)
     {
-        int x = ptr->endAdr-ptr->startAdr +1;
+        int x = ptr->endAdr - ptr->startAdr + 1;
         if (ptr->IsFree == 0 && size <= x)
             break;
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
     //allocate memory(change linked list )
-    ptr->IsFree=1;
+    ptr->IsFree = 1;
     int end = ptr->endAdr;
-    int y = ptr->startAdr+size-1;
-    ptr->endAdr=y;
-    node* NN = NewNode(0,y+1,end);
+    int y = ptr->startAdr + size - 1;
+    ptr->endAdr = y;
+    node *NN = NewNode(0, y + 1, end);
 
-    NN->next=ptr->next;
-    NN->prev=ptr;
-    if (ptr->next!=NULL)
-        ptr->next->prev=NN;
-    ptr->next=NN;
+    NN->next = ptr->next;
+    NN->prev = ptr;
+    if (ptr->next != NULL)
+        ptr->next->prev = NN;
+    ptr->next = NN;
 
     // return &memory[ptr->startAdr];
 }
 
-void MyFree(){
+void MyFree(int adr)
+{
+    node *ptr = start;
+    while (ptr->startAdr == adr && ptr != NULL)
+        ptr = ptr->next;
+    if (ptr == NULL)
+    {
+        printf("Invalid Argument or Given address not allocated");
+    }
+    else
+    {
+        ptr->IsFree = 0;
+        if (ptr->next != NULL)
+        {
+            if (ptr->next->IsFree == 0)
+            {
+                ptr->next->startAdr = ptr->startAdr;
 
+                ptr->next->prev = ptr->prev;
+                ptr->prev->next = ptr->next;
+            }
+        }
+    }
 }
-
-
