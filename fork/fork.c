7e9c196b96414385dcb7c68ce869f15e7d41cb9d
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2019-12-02 12:59:38 +0530

    fork 2

--- fork/fork.c
+++ fork/fork.c
@@ -3,5 +3,16 @@
 
 int main()
 {
+    if (fork())
+        printf("PARENT");
+    else
+        if(fork())
+            printf("CHILD 1");
+        else
+            printf("CHILD 2");
+        
+        
+    
+    
     
 }
